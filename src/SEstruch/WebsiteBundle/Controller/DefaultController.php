32b3c291892a309d99bdb8085ab12a223751dffa
Ricard Clau <ricard.clau@gmail.com>
2012-09-25 08:36:46 +0200

    mail is working now

--- src/SEstruch/WebsiteBundle/Controller/DefaultController.php
+++ src/SEstruch/WebsiteBundle/Controller/DefaultController.php
@@ -7,6 +7,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
     /**
@@ -29,6 +33,41 @@ class DefaultController extends Controller
     }
 
     /**
+     * @Route("/contact", name="contact_send", defaults={"_format"="json"})
+     * @Method("POST")
+     */
+    public function contactSendAction(Request $request)
+    {
+        $contactData = array(
+            'nombre' => $request->get('nombre'),
+            'email'  => $request->get('email'),
+            'mensaje'  => $request->get('mensaje'),
+        );
+
+        $errors = $this->validateContactData($contactData);
+
+        if (0 === count($errors)) {
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Missatge rebut des de la web www.sylviaestruch.com')
+                ->setFrom(array($contactData['email'] => $contactData['nombre']))
+                ->setTo('ricard.clau@gmail.com')
+                ->setBody($contactData['mensaje'])
+            ;
+
+            $this->get('mailer')->send($message);
+
+            return new Response(json_encode(array('msg' => $this->get('translator')->trans('contacto.mailok')), 200));
+        } else {
+            $jsonerr = array();
+            foreach ($errors as $error) {
+                $jsonerr[$error->getPropertyPath()][] = $app['translator']->trans($error->getMessage(), array(), 'validators');
+            }
+            
+            return new Response(json_encode(array('msg' => 'ERROR', 'errors' => $jsonerr), 400));
+        }
+    }
+
+    /**
      * @Route("/biography", name="biography")
      * @Template()
      * @Method("GET")
@@ -36,5 +75,26 @@ class DefaultController extends Controller
     public function biographyAction()
     {
         return array();
+    }
+
+    private function validateContactData(array $contactData)
+    {
+        $collectionConstraint = new Constraints\Collection(array(
+            'nombre' => array(
+                new Constraints\NotNull(),
+                new Constraints\NotBlank(),
+            ),
+            'email'  => array(
+                new Constraints\NotNull(),
+                new Constraints\NotBlank(),
+                new Constraints\Email()
+            ),
+            'mensaje' => array(
+                new Constraints\NotNull(),
+                new Constraints\NotBlank()
+            ),
+        ));
+
+        return $this->get('validator')->validateValue($contactData, $collectionConstraint);
     }
 }
